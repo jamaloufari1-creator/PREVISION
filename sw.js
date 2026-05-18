14dd971c3e99c3ad5f0ae46dc9d4808a53bcedf9
jamaloufari1-creator <jamal.oufari1@gmail.com>
2026-05-18 21:38:59 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-// OneSignal Service Worker
-importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
-
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', e => e.waitUntil(
-  caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
-));
-self.addEventListener('fetch', e => e.respondWith(fetch(e.request)));
