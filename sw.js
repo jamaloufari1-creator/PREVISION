616bffcf6ad2668c4bd19e7e888bcda26f69cd76
jamaloufari1-creator <jamal.oufari1@gmail.com>
2026-07-20 12:54:40 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,8 @@
+importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
+
+const VERSION = '2.0';
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', e => e.waitUntil(
+  caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+));
+self.addEventListener('fetch', e => e.respondWith(fetch(e.request)));
